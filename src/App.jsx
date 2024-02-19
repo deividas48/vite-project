807036cd233449af43e2ad5e89ebe773dcbb849b
deividas48 <deividas.v6@gmail.com>
2024-02-19 09:08:11 +0200

    some header

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,14 @@
 // import reactLogo from './assets/react.svg'
 // import viteLogo from '/vite.svg'
 import './App.css'
+import Header from './components/layout/Header'
 
 function App() {
   return (
+    <>
+    <Header />
     <div>test</div>
+    </>
   )
 }
 
---
+++ src/components/layout/Header.jsx
@@ -0,0 +1,7 @@
+export default function Header() {
+    <header className='bg-gray-900 text-white'>
+        <div className='container mx-auto flex justify-between items-center'>
+        <div>ShopTILLyouDrop</div>
+        </div>
+    </header>
+}
